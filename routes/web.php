f7475d134308c7a4effa062a4bf4e1e179bb09e1
diannuch <diannurcahyaf@gmail.com>
2022-10-17 17:06:15 +0700

    menyelesaikan struktur folder, routes dan view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Dian Nurcahya Ningrum",
+        "email" => "diannurcahya52@gmail.com",
+        "image" => "dian.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
