7acee064a1994ee0e4d667f289132fa6e3c64589
diannuch <diannurcahyaf@gmail.com>
2022-10-17 21:50:15 +0700

    menyelesaikan blade templating engine

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,15 +1,16 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Dian Blog | Posts</title>
-</head>
+@extends('layouts.main')
 
-<body>
-  <h1>Halaman Posts</h1>
-</body>
+@section('container')
+ @foreach ($posts as $posts)
+ <article class="mb-5"> 
+ <h2>
+  <a href="/posts/{{ $posts["slug"] }}">{{ $posts["title"] }}</a>
+</h2>
+ <h5>By : {{ $posts["author"] }}</h5>
+ <p>{{ $posts["body"] }}</p>
+ </article>
+     
+ @endforeach
+@endsection
 
-</html>
--- routes/web.php
+++ routes/web.php
@@ -14,17 +14,71 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
+    return view('home', [
+        "title" => "Home"
+    ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
+        "title" => "About",
         "name" => "Dian Nurcahya Ningrum",
         "email" => "diannurcahya52@gmail.com",
         "image" => "dian.png"
     ]);
 });
 
+
+
 Route::get('/blog', function () {
-    return view('posts');
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Dian Nurcahya Ningrum",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est rem eaque cumque dolore dolores nobis accusamus beatae! Et, nulla, nemo quia delectus, inventore id placeat ratione culpa accusamus natus sit labore necessitatibus sunt laudantium animi sint optio tenetur odit quisquam? Cupiditate, quia, animi iure corporis soluta recusandae vitae impedit autem facilis quaerat voluptatibus architecto dolorem! Aperiam ipsa facilis expedita, quae quod numquam sed voluptas consequuntur ipsum? Quod commodi magnam at facere velit, laudantium saepe fugiat quibusdam deleniti impedit ipsum aspernatur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Kim Seokjin",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsa dolorem pariatur iure repellat eligendi odit ducimus ea quis corporis, vero autem id, dolorum nesciunt voluptate tenetur cum atque, similique neque? Quaerat praesentium similique molestiae dolorum iste tenetur repellat quos provident deleniti. Molestias cupiditate doloribus quas exercitationem! Temporibus nobis quidem incidunt animi aliquid explicabo placeat vel minus officiis facere assumenda quasi molestias sunt sint dolorem sed labore ea, tempora vitae suscipit ullam amet. Quo, laboriosam iusto, nulla neque possimus omnis molestiae non nemo perspiciatis est sed nihil quasi ratione, ea cumque impedit ipsam nobis ullam? Ullam alias sequi sint veniam."
+        ],
+    ];
+    return view('posts', [
+        "title" => "Posts",
+        "posts" => $blog_posts
+    ]);
+});
+
+
+// Halaman single posts
+Route::get('posts/{slug}', function ($slug) {
+    $blog_posts = [
+        [
+            "title" => "Judul Post Pertama",
+            "slug" => "judul-post-pertama",
+            "author" => "Dian Nurcahya Ningrum",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est rem eaque cumque dolore dolores nobis accusamus beatae! Et, nulla, nemo quia delectus, inventore id placeat ratione culpa accusamus natus sit labore necessitatibus sunt laudantium animi sint optio tenetur odit quisquam? Cupiditate, quia, animi iure corporis soluta recusandae vitae impedit autem facilis quaerat voluptatibus architecto dolorem! Aperiam ipsa facilis expedita, quae quod numquam sed voluptas consequuntur ipsum? Quod commodi magnam at facere velit, laudantium saepe fugiat quibusdam deleniti impedit ipsum aspernatur."
+        ],
+        [
+            "title" => "Judul Post Kedua",
+            "slug" => "judul-post-kedua",
+            "author" => "Kim Seokjin",
+            "body" => "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ipsa dolorem pariatur iure repellat eligendi odit ducimus ea quis corporis, vero autem id, dolorum nesciunt voluptate tenetur cum atque, similique neque? Quaerat praesentium similique molestiae dolorum iste tenetur repellat quos provident deleniti. Molestias cupiditate doloribus quas exercitationem! Temporibus nobis quidem incidunt animi aliquid explicabo placeat vel minus officiis facere assumenda quasi molestias sunt sint dolorem sed labore ea, tempora vitae suscipit ullam amet. Quo, laboriosam iusto, nulla neque possimus omnis molestiae non nemo perspiciatis est sed nihil quasi ratione, ea cumque impedit ipsam nobis ullam? Ullam alias sequi sint veniam."
+        ],
+    ];
+
+    $new_post = [];
+    foreach ($blog_posts as $post) {
+        if ($post["slug"] === $slug) {
+            $new_post = $post;
+        }
+    }
+
+
+    return view('post', [
+        "title" => "Single Post",
+        "post" => $new_post
+    ]);
 });
