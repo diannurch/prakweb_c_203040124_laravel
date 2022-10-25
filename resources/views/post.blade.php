52a5e90088e2e18b41f723478bcf5c0ac43f0b49
diannuch <diannurcahyaf@gmail.com>
2022-10-25 12:01:16 +0700

    menyelesaikan video 8 - post category dan eloquent relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -4,7 +4,7 @@
 @section('container')
 <article>
   <h2 class="mb-5">{{ $post->title }}</h2>
-  <h5>{{ $post->author }}</h5>
+  <h5>By : {{ $post->author }} in  <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a> </h5>
   
   {!! $post->body !!}
 </article>
