a2bf58a58890f43f5cad118f515948123bf70343
diannuch <diannurcahyaf@gmail.com>
2022-10-30 23:51:04 +0700

    menyelesaikan video 12 - redesign ui

--- routes/web.php
+++ routes/web.php
@@ -20,13 +20,15 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        'active' => 'home',
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => "About",
+        'active' => 'about',
         "name" => "Dian Nurcahya Ningrum",
         "email" => "diannurcahya52@gmail.com",
         "image" => "dian.png"
@@ -44,6 +46,7 @@ Route::get('posts/{post:slug}', [PostController::class, 'show']);
 Route::get('/categories', function () {
     return view('categories', [
         'title' => 'Post Categories',
+        'active' => 'categories',
         'categories' => Category::all()
     ]);
 });
@@ -51,6 +54,7 @@ Route::get('/categories', function () {
 Route::get('/categories/{category:slug}', function (Category $category) {
     return view('posts', [
         'title' => "Post By Category : $category->name",
+        'active' => 'categories',
         'posts' => $category->posts->load('category', 'author'),
     ]);
 });
@@ -58,6 +62,7 @@ Route::get('/categories/{category:slug}', function (Category $category) {
 Route::get('/authors/{author:username}', function (User $author) {
     return view('posts', [
         'title' => "Post By Author : $author->name",
+        'active' => 'categories',
         'posts' => $author->posts->load('category', 'author'),
     ]);
 });
